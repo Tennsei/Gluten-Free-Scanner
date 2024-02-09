376ebd204011e7228c6cb475613b45c340985ff9
Arnav Juneja <arnavjuneja2007@gmail.com>
2024-02-09 00:02:06 -0800

    adding some basic changes to firebase_options

--- gluten_free_scanner/lib/firebase_options.dart
+++ gluten_free_scanner/lib/firebase_options.dart
@@ -2,7 +2,6 @@
 // ignore_for_file: lines_longer_than_80_chars, avoid_classes_with_only_static_members
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
-
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
@@ -18,18 +17,24 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      return web;
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web - '
+        'you can reconfigure this by running the FlutterFire CLI again.',
+      );
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
-        return android;
-      case TargetPlatform.iOS:
         throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for ios - '
+          'DefaultFirebaseOptions have not been configured for android - '
           'you can reconfigure this by running the FlutterFire CLI again.',
         );
+      case TargetPlatform.iOS:
+        return ios;
       case TargetPlatform.macOS:
-        return macos;
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
       case TargetPlatform.windows:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for windows - '
@@ -47,30 +52,12 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyCwQBWsPuniTPttJxSAhvsYgStnOOy0PyQ',
-    appId: '1:969329655343:web:251bcef98252168ff010da',
-    messagingSenderId: '969329655343',
-    projectId: 'gluten-free-scanner-4e23f',
-    authDomain: 'gluten-free-scanner-4e23f.firebaseapp.com',
-    storageBucket: 'gluten-free-scanner-4e23f.appspot.com',
-    measurementId: 'G-JT83PLBP16',
-  );
-
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyABfHbdSTaqhkeMqrZODcK6x1YPJ9PQVeY',
-    appId: '1:969329655343:android:a631b8174bcfb326f010da',
-    messagingSenderId: '969329655343',
-    projectId: 'gluten-free-scanner-4e23f',
-    storageBucket: 'gluten-free-scanner-4e23f.appspot.com',
-  );
-
-  static const FirebaseOptions macos = FirebaseOptions(
+  static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'AIzaSyCb-FLInSRLaQhl0Qk-KTGstzp_x6v7_0I',
-    appId: '1:969329655343:ios:43d2793e47fc0d93f010da',
+    appId: '1:969329655343:ios:f74e5e98611dce4af010da',
     messagingSenderId: '969329655343',
     projectId: 'gluten-free-scanner-4e23f',
     storageBucket: 'gluten-free-scanner-4e23f.appspot.com',
-    iosBundleId: 'com.example.glutenFreeScanner.RunnerTests',
+    iosBundleId: 'com.example.glutenFreeScanner',
   );
 }
